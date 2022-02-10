950f1d3c039b7d1a8c71c6f71eb2890abc638142
antonioliverjr <antoniobatistajr@gmail.com>
2022-02-10 16:17:58 -0300

    Definindo entidades não anêmicas.

--- CleanBase.Domain/Entities/Category.cs
+++ CleanBase.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanBase.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,31 @@ using System.Threading.Tasks;
 
 namespace CleanBase.Domain.Entities
 {
-    public class Category
+    public sealed class Category : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; private set; }
+        
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Id é invalido");
+            Id = id;
+            ValidateDomain(name);
+        }
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
         public ICollection<Product> Products { get; set; }
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name invalido. Name é required.");
+            DomainExceptionValidation.When(name.Length < 3, "Name invalido. Name deve conter no mínimo 3 caracteres.");
+            Name = name;
+        }
     }
 }
--- CleanBase.Domain/Entities/Product.cs
+++ CleanBase.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanBase.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,42 @@ using System.Threading.Tasks;
 
 namespace CleanBase.Domain.Entities
 {
-    public class Product
+    public sealed class Product : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Image { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Id é invalido");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name é invalido. Name é required");
+            DomainExceptionValidation.When(name.Length < 3, "Name invalido. Name deve conter no mínimo 3 caracteres.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Name é invalido. Name é required");
+            DomainExceptionValidation.When(description.Length < 5, "Name invalido. Name deve conter no mínimo 5 caracteres.");
+            DomainExceptionValidation.When(price < 0, "Price invalido. Deve ser diferente de zero.");
+            DomainExceptionValidation.When(stock < 0, "Price invalido. Deve ser diferente de zero.");
+            DomainExceptionValidation.When(image.Length > 250,
+                "Nome da Image invalido. Deve conter no máximo 250 caracteres.");
+        }
+
         public int CategoryId { get; set; }
         public Category Category { get; set; }
     }
