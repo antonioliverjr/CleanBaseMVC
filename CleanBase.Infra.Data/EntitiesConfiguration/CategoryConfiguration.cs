75568ee79fa2797cb60cedf8d441a7db242ecb05
antonioliverjr <antoniobatistajr@gmail.com>
2022-02-10 18:12:43 -0300

    Criação de Context com fluente API para configurar as criações no database.

---
+++ CleanBase.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanBase.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanBase.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(1, "Material Eletrônico"),
+                new Category(1, "Material Informática")
+            );
+        }
+    }
+}
