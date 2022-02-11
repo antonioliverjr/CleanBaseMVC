ea47da1f3ed7c6705062bdabb49b6c895a6c9b55
antonioliverjr <antoniobatistajr@gmail.com>
2022-02-11 09:36:41 -0300

    Classe de Inversão de Controle para desacoplamento entre as camadas, adicionando as dependências no WebUI.

---
+++ CleanBase.Infra.IoC/DependecyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanBase.Domain.Interfaces;
+using CleanBase.Infra.Data.Context;
+using CleanBase.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanBase.Infra.IoC
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services
+            , IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
+                , b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+            );
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            return services;
+        }
+    }
+}
