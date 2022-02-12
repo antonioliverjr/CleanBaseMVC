3001434d8ba4e420ef5742df480934b5ec15e65d
antonioliverjr <antoniobatistajr@gmail.com>
2022-02-12 19:38:21 -0300

    Criando as UI e ViewModels para o Login e Registro.

---
+++ CleanBase.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanBase.WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email é requerido.")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail valido.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password é requerido.")]
+        [StringLength(20, ErrorMessage = "O {0} deve conter entre {2} até {1}", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ CleanBase.WebUI/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanBase.WebUI.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email é requerido.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Senha é requerido.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Repita a senha, para confirmação.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "Senhas não conferem.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
