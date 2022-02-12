0505b8fb415d5fd9296bf6ad8bd12ef5b5a8e27c
antonioliverjr <antoniobatistajr@gmail.com>
2022-02-12 16:20:33 -0300

    implementando CRUD no controller e views do Produto.

--- CleanBase.WebUI/Controllers/ProductsController.cs
+++ CleanBase.WebUI/Controllers/ProductsController.cs
@@ -1,8 +1,9 @@
-﻿using CleanBase.Application.Interfaces;
+﻿using CleanBase.Application.DTOs;
+using CleanBase.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CleanBase.WebUI.Controllers
@@ -10,9 +11,14 @@ namespace CleanBase.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
+        public ProductsController(IProductService productService, ICategoryService categoryService,
+            IWebHostEnvironment environment)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _environment = environment;
         }
 
         [HttpGet]
@@ -20,6 +26,68 @@ namespace CleanBase.WebUI.Controllers
         {
             var products = await _productService.GetProducts();
             return View(products);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var product = await _productService.GetProductId(id);
+            if (product == null) return NotFound();
+
+            var wwwRoot = _environment.WebRootPath;
+            var image = Path.Combine(wwwRoot, "images\\" + product.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+            return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Categories = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Create(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var product = await _productService.GetProductId(id);
+            if (product == null) return NotFound();
+            ViewBag.Categories = new SelectList(await _categoryService.GetCategories(), "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var product = await _productService.GetProductId(id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _productService.Delete(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
