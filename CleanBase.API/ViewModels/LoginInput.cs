cbc12548796cd101332562214b05d6dd38fbf689
antonioliverjr <antoniobatistajr@gmail.com>
2022-02-13 17:27:55 -0300

    Criando as view models a ser usadas na criação do Token.

---
+++ CleanBase.API/ViewModels/LoginInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanBase.API.ViewModels
+{
+    public class LoginInput
+    {
+        [Required(ErrorMessage = "Email é requerido.")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail valido.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password é requerido.")]
+        [StringLength(20, ErrorMessage = "O {0} deve conter entre {2} até {1}", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ CleanBase.API/ViewModels/RegisterInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanBase.API.ViewModels
+{
+    public class RegisterInput
+    {
+        [Required(ErrorMessage = "Email é requerido.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Senha é requerido.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Repita a senha, para confirmação.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "Senhas não conferem.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
